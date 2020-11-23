b8fe4659c18c94bd1f139d31257152d40a215149
hri7566 <hri7566@gmail.com>
2020-11-22 23:10:42 -0500

    Add TypeScript bot

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Client = require('mpp-client-xt');
 const fs = require('fs');
 
-var pos = {x: 50, y: 50};
-var vel = {x: 2/5, y: 2/7};
-
 var client = new Client("wss://www.multiplayerpiano.com:443");
 
 client.start();
 
 stats = require('./stats.json');
+
+const DVD = require('./lib');
+var dvd = new DVD(stats)
 
 save = () => {
     fs.writeFile('./stats.json', JSON.stringify(stats), () => {});
@@ -19,37 +19,32 @@ client.on('hi', () => {
     client.setChannel("✧𝓡𝓟 𝓡𝓸𝓸𝓶✧");
 });
 
-cursorfunc = () => {
-    pos.x += vel.x;
-    pos.y += vel.y;
-    if ((pos.x >= 100 && vel.x > 0) || (pos.x <= 0 && vel.x < 0)) {
-        vel.x = -vel.x;
-        stats.w += 1;
-        save();
-    }
-    if ((pos.y >= 100 && vel.y > 0) || (pos.y <= 0 && vel.y < 0)) {
-        vel.y = -vel.y;
-        stats.w += 1;
-        save();
-    }
-    if ((pos.x <= 0 && pos.y <= 0) || (pos.x >= 100 && pos.y <= 0) || (pos.x >= 100 && pos.y >= 100) || (pos.x <= 0 && pos.y >= 100)) {
-        stats.c += 1;
-        client.sendArray([{m: 'a', message: "Corner hit!"}]);
-        save();
-    }
-    client.sendArray([{m: 'm', x: pos.x, y: pos.y}]);
-}
-
-cursor = setInterval(cursorfunc, 25);
+cursor = setInterval(() => {dvd.update()}, 25);
 
 client.on('a', msg => {
     let args = msg.a.split(' ');
     let cmd = args[0].toLowerCase();
     let argcat = msg.a.substring(cmd.length).trim();
     if (cmd == "dvd!stats") {
-        client.sendArray([{m:'a', message:`┐ Corner hits: ${stats.c} | Wall hits: ${stats.w}`}]);
+        client.sendArray([{m:'a', message:`┐ Corner hits: ${dvd.stats.c} | Wall hits: ${dvd.stats.w}`}]);
     }
 });
+
+dvd.onUpdate = () => {
+    client.sendArray([{m:'m', x: dvd.pos.x, y: dvd.pos.y}]);
+}
+
+dvd.onCornerHit = () => {
+    client.sendArray([{m:'a', message:`Corner hit!`}]);
+}
+
+dvd.save = () => {
+    fs.writeFile(__dirname+"/stats.json", JSON.stringify(dvd.stats), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
 
 cursorOn = () => {
     if (cursor) return false;
